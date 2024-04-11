83e97f812e26819eb5ec92808b859c4fadf3784c
JesusTM <jesustapiamartin@gmail.com>
2024-04-11 09:45:38 -0400

    Alternativa modular del código de «Compra y Venta»

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,135 +2,203 @@ import java.util.Scanner;
 
 public class Main {
 
+    // ===== MÉTODO MAIN =====
     public static void main(String[] args) {
         menu();
     }
 
+
+
+    // ===== ARREGLO =====
     public static String[][] inventario = new String[10][2];
+    public static int largoInventario   = inventario.length;
 
-    public static void menu() {
 
-        int continuar = 0;
 
-        do {
-            mostrarOpciones();
-            String opcion = scanner.nextLine();
-
-            switch(opcion){
-                case "1":
-                    menuAgregarProducto();
-                    break;
-                case "2":
-                    menuVenderProducto();
-                    break;
-                case "3":
-                    mostrarInventario(inventario);
-                    break;
-                case "4":
-                    System.out.println("Saliendo del programa");
-                    continuar = 1;
-                default:
-                    System.out.println("Opción no válida");
+    // ===== VALIDAR CANTIDAD =====
+    public static boolean validarCantidad(String cantidadProducto) {
+        try {
+            int cantidad = Integer.parseInt(cantidadProducto);
+            if (cantidad <= 0) {
+                System.out.println("La cantidad del producto debe ser un número positivo");
+                return false;
             }
-        } while(continuar == 0);
-
-
-    }
-
-    public static void mostrarOpciones(){
-        System.out.println("[1] Agregar Producto");
-        System.out.println("[2] Vender Producto");
-        System.out.println("[3] Mostrar Inventario");
-        System.out.println("[4] Salir");
-        System.out.println("Ingrese una opción: ");
-    }
-
-    public static void agregarPoducto(String [][] matrizInventario, String nombreProducto, String cantidadProducto){
-
-        for(int i = 0; i < matrizInventario.length; i++){
-            if(matrizInventario[i][0] == null){
-                matrizInventario[i][0] = nombreProducto;
-                matrizInventario[i][1] = cantidadProducto;
-                System.out.println("Producto agregado correctamente");
-                break;
-            }
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("El stock del producto debe ser un número válido");
+            return false;
         }
     }
 
-    public static void venderProducto(String[][] matrizInventario, int posicion, int cantidad) {
 
-        String stock = matrizInventario[posicion][1];
-        int cantidadActual = Integer.parseInt(stock);
+
+    // ===== AGREGAR PRODUCTO =====
+    public static void agregarProducto(String nombreProducto, String cantidadProducto) {
+        if (!validarCantidad(cantidadProducto)) {
+            return;
+        }
+        for (int i = 0; i < largoInventario; i++) {
+            if (inventario[i][0] == null) {
+                inventario[i][0] = nombreProducto;
+                inventario[i][1] = cantidadProducto;
+                System.out.println("El producto " + nombreProducto + " fue agregado correctamente");
+                return;
+            }
+        }
+        System.out.println("Error, inventario lleno");
+    }
+
+
+
+    // ===== SOLICITAR DATOS PRODUCTO =====
+    public static String[] solicitarDatosProducto() {
+        Scanner lector          = new Scanner(System.in);
+        String[] datosProducto  = new String[2];
+
+        System.out.println("\n ----- AGREGAR PRODUCTO ----- ");
+        System.out.print  ("    Nombre  : ");
+        datosProducto[0] = lector.nextLine();
+
+        System.out.print  ("    Cantidad: ");
+        datosProducto[1] = lector.nextLine();
+
+        return datosProducto;
+    }
+
+
+
+    // ===== MENÚ AGREGAR PRODUCTO =====
+    public static void agregarProductoMenu() {
+        String[] datosProducto  = solicitarDatosProducto();
+        String nombre           = datosProducto[0];
+        String cantidad         = datosProducto[1];
+
+        try {
+            Integer.parseInt(cantidad);
+            agregarProducto(nombre, cantidad);
+        } catch (NumberFormatException e) {
+            System.out.println("El stock del producto debe ser un número");
+        }
+    }
+
+
+
+    // ===== SOLICITAR DATOS VENTA =====
+    public static String[] solicitarDatosVenta() {
+        Scanner lector      = new Scanner(System.in);
+        String[] datosVenta = new String[2];
+
+        System.out.println("\n ----- VENDER PRODUCTO ----- ");
+        System.out.print  ("    Nombre  : ");
+        datosVenta[0] = lector.nextLine();
+
+        System.out.print  ("    Cantidad: ");
+        datosVenta[1] = lector.nextLine();
+
+        return datosVenta;
+    }
+
+
+
+    // ===== MENÚ VENDER PRODUCTO =====
+    public static void menuVenderProducto() {
+        String[] datosVenta     = solicitarDatosVenta();
+        String nombreProducto   = datosVenta[0];
+        String cantidadProducto = datosVenta[1];
+
+        try {
+            int cantidad = Integer.parseInt(cantidadProducto);
+            if (validarCantidad(cantidadProducto) == false) {
+                return;
+            }
+
+            int posicion = buscarProducto(nombreProducto);
+
+            if (posicion != -1) {
+                postVenta(posicion, cantidad);
+                System.out.println("Venta realizada correctamente");
+            } else {
+                System.out.println("El producto no existe");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("La cantidad debe ser un número");
+        }
+    }
+
+
+
+    // ===== POST VENTA =====
+    public static void postVenta(int posicion, int cantidad) {
+        String stock        = inventario[posicion][1];
+        int cantidadActual  = Integer.parseInt(stock);
 
         if (cantidadActual >= cantidad) {
-            matrizInventario[posicion][1] = String.valueOf(cantidadActual - cantidad);
-            System.out.println("Venta realizada correctamente");
+            inventario[posicion][1] = String.valueOf(cantidadActual - cantidad);
         } else {
             System.out.println("No hay suficiente cantidad de producto");
         }
-
     }
 
-    public static void mostrarInventario(String[][] matrizInventario) {
 
-        for (int i = 0; i < matrizInventario.length; i++) {
-            if (matrizInventario[i][0] != null) {
-                System.out.println("Producto: " + matrizInventario[i][0] + " - Cantidad: " + matrizInventario[i][1]);
+
+    // ===== MOSTRAR INVENTARIO =====
+    public static void mostrarInventario() {
+        System.out.println("\n    ----- INVENTARIO ------ ");
+        for (int i = 0; i < largoInventario; i++) {
+            if (inventario[i][0] != null) {
+                System.out.println("Producto: " + inventario[i][0] + " - Cantidad: " + inventario[i][1]);
             }
         }
     }
 
-    public static int buscarProducto(String[][] matrizInventario, String nombreProducto) {
 
-        for (int i = 0; i < matrizInventario.length; i++) {
-            if (matrizInventario[i][0] != null && matrizInventario[i][0].equals(nombreProducto)) {
+
+    // ===== BUSCAR PRODUCTO =====
+    public static int buscarProducto(String nombreProducto) {
+        for (int i = 0; i < largoInventario; i++) {
+            if (inventario[i][0] != null && inventario[i][0].equals(nombreProducto)) {
                 return i;
             }
         }
-
         return -1;
     }
 
-    public static void menuAgregarProducto(){
 
-        System.out.println("Ingrese el nombre del producto");
-        String nombreProducto = scanner.nextLine();
 
-        System.out.println("Ingrese la cantidad del producto");
-        String cantidadProducto = scanner.nextLine();
+    // ===== MENÚ PRINCIPAL =====
+    public static void menu () {
+        Scanner lector = new Scanner(System.in);
+        String opcion;
 
-        try {
-            Integer.parseInt(cantidadProducto);
-            agregarPoducto(inventario, nombreProducto, cantidadProducto);
-        } catch (NumberFormatException e) {
-            System.out.println("El stock debe ser un numero");
-        }
-
-    }
-
-    public static void menuVenderProducto(){
-
-        System.out.println("Ingrese el nombre del producto");
-        String nombreProducto = scanner.nextLine();
-
-        int posicion = buscarProducto(inventario, nombreProducto);
-
-        if (posicion != -1) {
-
-            System.out.println("Ingrese la cantidad del producto");
-            String cantidadProducto = scanner.nextLine();
-
-            try {
-                int cantidad = Integer.parseInt(cantidadProducto);
-                venderProducto(inventario, posicion, cantidad);
-            } catch (NumberFormatException e) {
-                System.out.println("La cantidad debe ser un numero");
+        do {
+            mostrarMenuPrincipal();
+            opcion = lector.nextLine();
+            switch (opcion){
+                case "1" -> agregarProductoMenu();
+                case "2" -> {
+                    mostrarInventario();
+                    menuVenderProducto();
+                }
+                case "3" -> mostrarInventario();
+                case "4" -> System.out.println(" Hasta luego...");
+                default  -> System.out.println(" Opción no válida. Por favor, ingrese una opción válida.");
             }
-
-        } else {
-            System.out.println("El producto no existe");
-        }
+        } while (!opcion.equals("4"));
     }
-    public static Scanner scanner = new Scanner(System.in);
 
+
+
+    // ===== MÉTODO PARA MOSTRAR MENU PRINCIPAL =====
+    public static void mostrarMenuPrincipal() {
+        System.out.println("\n======================================");
+        System.out.println("            Compra y Venta            ");
+        System.out.println("               La Granja              ");
+        System.out.println("======================================");
+        System.out.println("        [1] Agregar producto          ");
+        System.out.println("        [2] Vender producto           ");
+        System.out.println("        [3] Mostrar inventario        ");
+        System.out.println("        [4] Salir                     ");
+        System.out.println("======================================");
+        System.out.print  ("            Ingrese su opción  : ");
+    }
 }
